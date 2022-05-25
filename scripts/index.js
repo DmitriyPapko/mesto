1dde87c4308c2c1981d2087387ebd3259c6b6266
DmitriyPapko <papko.dmitrii@gmail.com>
2022-05-25 18:17:17 +0300

    3

--- scripts/index.js
+++ scripts/index.js
@@ -5,28 +5,25 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const popupInputName = document.querySelector('.popup__input_js_name');
 const popupInputJob = document.querySelector('.popup__input_js_job');
-const popupSaveBtn = document.querySelector('.popup__save');
-const formElement = document.querySelector('.popup__container');
+const formElement = document.querySelector('.popup__form');
 
 function openPopup(){
    popupInputName.value = profileTitle.textContent;
    popupInputJob.value = profileSubtitle.textContent;
    popup.classList.add('popup_opened');
-   openPopup(popup)
 }
 
 function closePopup(){
-   profileTitle.textContent = popupInputName.value;
-   profileSubtitle.textContent = popupInputJob.value;
    popup.classList.remove('popup_opened');
-   closePopup(popup);
 }
 
-popupSaveBtn.onclick = function(evt){
-   evt.preventDefault();
-   closePopup(popup);
+function formSubmitHandler (evt) {
+   evt.preventDefault(); 
+   profileTitle.textContent = popupInputName.value;
+   profileSubtitle.textContent = popupInputJob.value ;
+   closePopup();
 }
 
 editPopup.addEventListener('click', openPopup);
 closePopupBtn.addEventListener('click', closePopup);
-popupSaveBtn.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
